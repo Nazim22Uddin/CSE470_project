d63ed4ead823bd0795126d10ea73b33a10ff426f
Nazim22Uddin <98313773+Nazim22Uddin@users.noreply.github.com>
2023-05-10 11:43:48 +0600

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,13 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$conn = mysqli_connect("$servername","$username","$password");
 
-$con = mysqli_connect("localhost","root","","ecom_store");
-
+if (!$conn){
+    die("Sorry we failed to connect: ". mysqli_connect_error());
+}
+else{
+    echo "Connection was successful";
+}
 ?>
